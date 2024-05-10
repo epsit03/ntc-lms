6a81041e3a7594bc5e94ca995214cf33ae70ca97
omdwd74 <omjidwivedi123@gmail.com>
2024-05-11 03:30:57 +0530

    deployement

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,9 @@
-const { connect } = require("mongoose")
+// const { connect } = require("mongoose")
 
-const connectDb = async () => {
-  return connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
-}
+// const connectDb = async () => {
+//   return connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
+// }
+const mongoose = require("mongoose");
+ const connectDb = mongoose.connect("mongodb+srv://omjidwivedi49:Omji%40123@cluster0.vdeaofl.mongodb.net/NTC-LMS?retryWrites=true&w=majority&appName=Cluster0")
 
 module.exports = { connectDb }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,17 @@ import { BrowserRouter as Router } from "react-router-dom"
 import { Container } from "@mui/material"
 import { NotificationContainer } from "react-notifications"
 import { AppLayout } from "./components/layout/app-layout"
-// import {LandingPage} from "./components/landing_page/LandingPage"
 import { UserProvider } from "./context/user-context"
 
 export const App = () => (
   <UserProvider>
     <Suspense fallback={null}>
-      <Container className="page-container">
+      {/* <Container className="page-container"> */}
         <Router>
-          {/* <LandingPage/> */}
           <AppLayout />
           <NotificationContainer />
         </Router>
-      </Container>
+      {/* </Container> */}
     </Suspense>
   </UserProvider>
 )
